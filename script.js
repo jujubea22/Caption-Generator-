e63c43a42ad1ef93579346136f4a82824e91f0f4
jujubea22 <114043890+jujubea22@users.noreply.github.com>
2022-11-09 02:16:10 -0500

    Update script.js
    Here is the words API - running into issues

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 // 
+
+<p1>Click the button to get suggestions of other words</p1>
+
+<button onclick="getWords('https://www.wordsapi.com/')">Find Similar Words</button>
+<script>
+
+
+async function getText(file) {
+  let myObject = await fetch(file);
+  let myText = await myObject.text();
+  document.getElementById("demo").innerHTML = myText;
+}
