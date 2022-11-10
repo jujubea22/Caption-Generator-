7464fa8fdf047167755dc8298e0f0b84f98b9492
tatixdemi <tatixdemi@gmail.com>
2022-11-09 19:54:37 -0500

    push code

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 // 
+var friendship = [
+    caption 1
+    caption 2 
+    caption 3
+]
+function newCaption() {
+    var randomNumber = Math.floor(Math.random() * (friendship.length));
+    document.getElementById('situationBtn').innerHTML = friendship[randomNumber];
+}
